c8eb8f31f1b38329ecf759e9e1da34d6fa3fd844
Nick LaMuro <nicklamuro@gmail.com>
2014-06-23 09:24:27 -0500

    Adds base structure for gli bin

--- lib/octopolo.rb
+++ lib/octopolo.rb
@@ -1,16 +1,9 @@
-# Trap interrupts (e.g., Ctrl-C) to quit cleanly, without a stack trace.
-# -- lifted from vagrant's source
-Signal.trap("INT") do
-  puts ""
-  puts "Exiting..."
-  exit 1
-end
-
 require "readline"
 require "octokit"
 require "hashie"
 require_relative "octopolo/cli"
 require_relative "octopolo/config"
+require_relative "octopolo/version"
 require_relative "octopolo/convenience_wrappers"
 
 module Octopolo
