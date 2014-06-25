0cc574bb6fa76a2769db5806e4ef62c9bd0f957a
Nick LaMuro <nicklamuro@gmail.com>
2014-06-25 11:21:20 -0500

    Fixes typo with new-branch

--- lib/octopolo/scripts/new_branch.rb
+++ lib/octopolo/scripts/new_branch.rb
@@ -4,7 +4,7 @@ require_relative "../git"
 arg :new_branch_name,    :name => 'new_branch_name'
 arg :source_branch_name, :name => 'source_branch_name', :optional => true
 desc 'Create a new branch for features, bug fixes, or experimentation.'
-command 'deployable' do |c|
+command 'new-branch' do |c|
   c.action do |global_options, options, args|
     Octopolo::Scripts::NewBranch.execute args[0], args[1]
   end
