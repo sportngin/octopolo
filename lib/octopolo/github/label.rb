f20d9fc93a91de3cf9e403a2d0381c3b7d87afff
elliothursh <elliothursh@gmail.com>
2014-07-23 11:28:04 -0500

    refactored add_to_pull method

--- lib/octopolo/github/label.rb
+++ lib/octopolo/github/label.rb
@@ -16,7 +16,7 @@ module Octopolo
       end
 
       def == (obj)
-        return true if ((self.name == obj.name) && (self.color == obj.color))
+        (self.name == obj.name) ? true : false
       end
 
       # Public: Grabs all labels from either file or github
@@ -25,38 +25,61 @@ module Octopolo
         from_github
       end
 
+      # Public: Gets the names of labels
+      #
+      # label_array - an array of labels
+      #
+      # returns - an array of all names from label_array
+      def self.get_names(label_array)
+        label_array.map{ |label| label.name }
+      end
+
       # Public: Checks to see if label exists on remote, if not makes one.
+      #
+      # label - a label object
       def self.first_or_create(label)
-        if !(all_labels.include? label)
+        unless all_labels.include?(label)
           GitHub.add_label(config.github_repo, label.name, label.color)
         end
       end
 
       # Public: Adds labels to a pull-request
       #
-      # labels: an array of labels
-      # pull_number: number of the pull_request to add label to
-      def self.add_to_pull(pull_number, labels)
-        if !(labels.is_a? Array)
-          labels = [labels]
-        end
-
-        labels.each {|label| first_or_create(label) }
-        GitHub.add_labels_to_pull(config.github_repo, pull_number, labels.map{ |label| label.name } )
+      # labels - an array of labels
+      # pull_number - number of the pull_request to add label to
+      def self.add_to_pull(pull_number, *labels)
+        label_names = get_names(build_label_array(labels))
+        GitHub.add_labels_to_pull(config.github_repo, pull_number, label_names )
       end
 
       # Private: takes in a hash, out puts a label
-      #  
+      # 
+      # label_hash - a hashed label
+      #
+      # returns - a label
       def self.to_label(label_hash)
         Label.new(label_hash[:name], label_hash[:color])
       end
       private_class_method :to_label
 
       # Private: Gets all the labels from given repository on github
+      #
+      # returns - an array of labels
       def self.from_github
         GitHub.labels(config.github_repo).map{ |label_hash| to_label(label_hash) }
       end
       private_class_method :from_github
+
+      # Private: Finds or creates each of the passed in labels
+      #
+      # labels - label objects, can be a single label, an array of labels,
+      #          or a list of labels
+      # 
+      # returns - an array of labels.
+      def self.build_label_array(*labels)
+        Array(labels).flatten.each {|label| first_or_create(label)}
+      end
+      private_class_method :build_label_array
 
     end
   end
--- spec/octopolo/github/label_spec.rb
+++ spec/octopolo/github/label_spec.rb
@@ -46,20 +46,58 @@ module Octopolo
         end
       end
 
+      context "#==" do
+        it "returns true if names are same ignoring color" do
+          expect(label1 == Label.new("low-risk","121212")).to eq(true)
+        end
+
+        it "returns true if names are same ignoring color" do
+          expect(label1 == label2).to eq(false)
+        end
+      end
+
       context "#add_to_pull" do
         let (:pull_number) {007}
         it "sends the correct arguments to add_labels_to_pull for multiple labels" do
-          allow(Label).to receive(:first_or_create).and_return([label1, label2])
+          allow(Label).to receive(:first_or_create)
           expect(GitHub).to receive(:add_labels_to_pull).with(config.github_repo, pull_number, ["low-risk","high-risk"])
           Label.add_to_pull(pull_number,[label1, label2])
         end
 
         it "sends the correct arguments to add_labels_to_pull for a single label" do
-          allow(Label).to receive(:first_or_create).and_return(label1)
+          allow(Label).to receive(:first_or_create)
           expect(GitHub).to receive(:add_labels_to_pull).with(config.github_repo, pull_number, ["low-risk"])
           Label.add_to_pull(pull_number,label1)
         end
-      end  
+      end 
+
+      context "#build_labels" do 
+        it "returns an array of label when given a label" do
+          allow(Label).to receive(:first_or_create)
+          expect(Label.send(:build_label_array,label1)).to eq([label1])
+        end
+
+        it "returns an array of labels when given a list of labels" do
+          allow(Label).to receive(:first_or_create)
+          expect(Label.send(:build_label_array,label1,label2)).to eq([label1,label2])
+        end
+
+        it "returns an array of labels when given an array of length 1" do
+          allow(Label).to receive(:first_or_create)
+          expect(Label.send(:build_label_array,[label1])).to eq([label1])
+        end
+
+        it "returns an array of labels when given an array of length 1" do
+          allow(Label).to receive(:first_or_create)
+          expect(Label.send(:build_label_array,[label1,label2])).to eq([label1,label2])
+        end
+      end
+
+      context "#get_names" do
+        it "returns a list of names when given an array of labels" do
+          expect(Label.get_names([label1,label2])).to eq(["low-risk","high-risk"])
+        end
+      end
     end
   end
 end
