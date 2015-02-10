98de2b79061400ece248a74f16af616b002448f1
Nick LaMuro <nicklamuro@gmail.com>
2015-02-10 17:44:25 -0600

    Allows editor to be set in the user config
    Since using the editor will be based on personal preference, and the
    preference will most likely be something that users will want one or the
    other, this allows that preference to be set in the user config of
    octopolo.

--- lib/octopolo/commands/pull_request.rb
+++ lib/octopolo/commands/pull_request.rb
@@ -1,12 +1,13 @@
 desc "Create a pull request from the current branch to the application's designated deploy branch."
 command 'pull-request' do |c|
   config = Octopolo::Config.parse
+  user_config = Octopolo::UserConfig.parse
 
   c.desc "Branch to create the pull request against"
   c.flag [:d, :dest, :destination], :arg_name => "destination_branch", :default_value => config.deploy_branch
 
   c.desc "Use $EDITOR to update PR description before creating"
-  c.switch [:e, :editor], :default_value => false
+  c.switch [:e, :editor], :default_value => user_config.editor
 
   c.action do |global_options, options, args|
     require_relative '../scripts/pull_request'
