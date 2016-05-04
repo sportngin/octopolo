33d4152e14794c0b2181878dda3e97b40de204e1
Brian Bergstrom <boilingbergstrom@gmail.com>
2016-05-04 15:32:00 -0500

    Revert "View PR in Browser"

--- lib/octopolo/commands/view_pr.rb
+++
@@ -1,7 +0,0 @@
-desc 'Opens the Pull Request in your default browser'
-command 'view-pr' do |c|
-  c.action do |global_options, options, args|
-    require_relative '../scripts/view_pr'
-    Octopolo::Scripts::ViewPr.execute
-  end
-end
