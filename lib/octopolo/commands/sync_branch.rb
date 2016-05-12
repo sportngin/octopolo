1d9784ecdb30c8692add52ffe13277fdffb61861
Chris Arcand <chris@chrisarcand.com>
2016-05-12 18:10:10 -0400

    Use perfectly good module accessors

--- lib/octopolo/commands/sync_branch.rb
+++ lib/octopolo/commands/sync_branch.rb
@@ -1,8 +1,7 @@
-config = Octopolo::Config.parse
-long_desc "branch - Which branch to merge into yours (default: #{config.deploy_branch})"
+long_desc "branch - Which branch to merge into yours (default: #{Octopolo.config.deploy_branch})"
 
 arg :branch
-desc "Merge the #{config.deploy_branch} branch into the current working branch"
+desc "Merge the #{Octopolo.config.deploy_branch} branch into the current working branch"
 command 'sync-branch' do |c|
   c.action do |global_options, options, args|
     require_relative '../scripts/sync_branch'
