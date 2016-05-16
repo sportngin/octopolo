34f97fc6cef85ba274605677ea3e405e8eb7de96
Brian Bergstrom <brian.bergstrom@sportngin.com>
2016-05-16 12:46:15 -0500

    Fix existing tests.

--- spec/octopolo/scripts/new_branch_spec.rb
+++ spec/octopolo/scripts/new_branch_spec.rb
@@ -6,19 +6,29 @@ module Octopolo
     describe NewBranch do
       let(:config) { stub(:config, :deploy_branch => "production") }
       let(:git) { stub(:Git) }
+      let(:cli) { stub(:Cli) }
       let(:new_branch_name) { stub(:string) }
       let(:custom_source_branch) { stub(:string) }
 
       subject { NewBranch }
 
       before do
-        NewBranch.any_instance.stub(:config => config, :git => git)
+        NewBranch.any_instance.stub(:config => config, :git => git, :cli => cli)
       end
 
       context "::execute" do
         context "with no arguments given" do
           it "fails if given no arguments" do
             expect { subject.execute }.to raise_error(ArgumentError)
+          end
+        end
+
+        context "with a only new branch name given" do
+          it "delegates to Git.new_branch" do
+            allow(git).to receive(:reserved_branch?) { true }
+            allow(cli).to receive(:ask_boolean) { false }
+            git.should_receive(:new_branch).with(new_branch_name, "staging-test")
+            subject.execute(new_branch_name)
           end
         end
 
